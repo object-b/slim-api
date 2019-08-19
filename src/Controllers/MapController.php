8cc5b9c51b960a69caa9c0ccea651cc8363ef223
Roman Vlasenko <rvlasenko.1997@gmail.com>
2019-08-19 12:57:55 +0400

    Get markers by radius 📻

--- src/Controllers/MapController.php
+++ src/Controllers/MapController.php
@@ -11,6 +11,7 @@ use App\Models\Object\Description;
 use App\Models\Object\Event;
 use App\Models\User\BaseUser;
 use Faker\Factory;
+use Illuminate\Support\Facades\DB;
 
 class MapController
 {
@@ -23,9 +24,69 @@ class MapController
 
     public function getByRadius($request, $response, $args)
     {
-        $data = [];
+        $sourceLat = $request->getParam('lat');
+        $sourceLon = $request->getParam('lng');
+        $radiusKm = $request->getParam('radius');
+        $proximity = $this->mathGeoProximity($sourceLat, $sourceLon, $radiusKm);
+        
+        $addresses = Address::whereBetween('latitude', [
+            $proximity['latitudeMin'], $proximity['latitudeMax'],
+        ])->whereBetween('longitude', [
+            $proximity['longitudeMin'], $proximity['longitudeMax'],
+        ])->get();
 
-        return $response->withJson($data, 200);
+        // Сравнить все записи и получить только те, которые действительно находятся в радиусе
+        $objectsWithinRadius = [];
+
+        foreach ($addresses as $address) {
+            $distance = $this->mathGeoDistance($sourceLat, $sourceLon, $address->latitude, $address->longitude);
+            
+            if ($distance <= $radiusKm) {
+                $objectsWithinRadius[] = [
+                    $address->latitude,
+                    $address->longitude,
+                    $address->city,
+                ];
+            }
+        }
+
+        return $response->withJson($objectsWithinRadius, 200);
+    }
+
+    // Вычислить географическую дистанцию между двумя точками
+    protected function mathGeoDistance($lat1, $lng1, $lat2, $lng2, $miles = false)
+    {
+        $pi80 = M_PI / 180;
+        $lat1 *= $pi80;
+        $lng1 *= $pi80;
+        $lat2 *= $pi80;
+        $lng2 *= $pi80;
+
+        $r = 6372.797;
+        $dlat = $lat2 - $lat1;
+        $dlng = $lng2 - $lng1;
+        $a = sin($dlat / 2) * sin($dlat / 2) + cos($lat1) * cos($lat2) * sin($dlng / 2) * sin($dlng / 2);
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+        $km = $r * $c;
+
+        return ($miles ? ($km * 0.621371192) : $km);
+    }
+
+    protected function mathGeoProximity($latitude, $longitude, $radius, $miles = false)
+    {
+        $radius = $miles ? $radius : ($radius * 0.621371192);
+
+        $lng_min = $longitude - $radius / abs(cos(deg2rad($latitude)) * 69);
+        $lng_max = $longitude + $radius / abs(cos(deg2rad($latitude)) * 69);
+        $lat_min = $latitude - ($radius / 69);
+        $lat_max = $latitude + ($radius / 69);
+
+        return [
+            'latitudeMin'  => number_format($lat_min, 6, '.', ''),
+            'latitudeMax'  => number_format($lat_max, 6, '.', ''),
+            'longitudeMin' => number_format($lng_min, 6, '.', ''),
+            'longitudeMax' => number_format($lng_max, 6, '.', ''),
+        ];
     }
 
     public function fakeCreate()
@@ -38,12 +99,18 @@ class MapController
         $create_address = 0;
         $create_desc = 0;
         $create_event = 0;
-        
+
         $user_data = [];
         $object_data = [];
         $address_data = [];
         $desc_data = [];
         $event_data = [];
+        $dates = [];
+
+        for ($i = 0; $i < 100; $i++) { 
+            $dates[$i] = $faker->dateTimeBetween('-5 years');
+        }
+        sort($dates);
 
         if ($create_users) {
             for ($i = 0; $i < $records; $i++) {
@@ -51,7 +118,7 @@ class MapController
                     'name' => $faker->firstName . ' ' . $faker->lastName,
                     'api_key' => $faker->randomNumber(7),
                     'email' => $faker->freeEmail,
-                    'created_at' => $faker->dateTime(),
+                    'created_at' => $dates[$i],
                 ];
             }
 
@@ -66,8 +133,8 @@ class MapController
                     'creator_id' => $user_ids[$i],
                     'points' => 0,
                     'object_status_id' => ObjectStatus::PUBLISHED,
-                    'created_at' => $faker->dateTime(),
-                    'updated_at' => $faker->dateTime(),
+                    'created_at' => $dates[$i],
+                    'updated_at' => $dates[$i],
                 ];
             }
 
@@ -82,7 +149,7 @@ class MapController
                 'display_name' => $faker->address,
                 'city' => $faker->city,
                 'state' => $faker->region . ' ' . $faker->regionSuffix,
-                'latitude' => $faker->latitude(54.2924, 54.3874),
+                'latitude' => $faker->latitude(54.2924, 54.4874),
                 'longitude' => $faker->longitude(48.2918, 48.6558),
                 'city_district' => 'Заволжский район', // Городской район, может быть пустым
                 'county' => '', // Округ, может быть пустым
@@ -112,7 +179,7 @@ class MapController
                 'user_id' => $user_ids[$i],
                 'object_status_id' => ObjectStatus::PUBLISHED,
                 'object_description_id' => $desc_ids[$i],
-                'created_at' => $faker->dateTime(),
+                'created_at' => $dates[$i],
             ];
         }
 
